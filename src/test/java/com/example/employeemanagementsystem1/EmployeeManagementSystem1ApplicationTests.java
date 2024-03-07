ce06e9ee977237141091a1a1528bf7a6b24c0003
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 16:19:07 +0530

    first commit

--- src/test/java/com/example/employeemanagementsystem1/EmployeeManagementSystem1ApplicationTests.java
+++ src/test/java/com/example/employeemanagementsystem1/EmployeeManagementSystem1ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.employeemanagementsystem1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EmployeeManagementSystem1ApplicationTests {
 
 	@Test
