768088bbfcafc18119282d9870da1e7d313aa607
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 16:24:13 +0530

    first commit

--- src/test/java/com/example/employeemanagementsystem1/repisitory/EmployeeRepositoryTests.java
+++ src/test/java/com/example/employeemanagementsystem1/repisitory/EmployeeRepositoryTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 public class EmployeeRepositoryTests {
 
